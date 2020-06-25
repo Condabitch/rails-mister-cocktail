586e6f6d4edad8715584d4d098be175db3ff00bd
julien condamines <j.condamines@gmail.com>
2020-06-25 18:34:46 +0800

    new cocktail view working

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,27 @@
+class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    if @dose.save
+      redirect_to cocktail_path(@dose.cockail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    flash[:success] = "Great! The dose Has been destroyed!"
+    redirect_to cocktail_path(@dose.cockail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:ingredient.name)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lime")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "sparkling water")
+Ingredient.create(name: "Angostura")
+Ingredient.create(name: "Gin")
+Ingredient.create(name: "Whisky")
+Ingredient.create(name: "Rhum")
+Ingredient.create(name: "Tonic Water")
+Ingredient.create(name: "Brown Sugar")
+Ingredient.create(name: "Water")
+Ingredient.create(name: "Orange slice")
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Old Fashioned")
+Cocktail.create(name: "Herbal G&T")
